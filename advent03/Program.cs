1e6303c5b85eed7e0a721e93b4122b5434475a85
grajekf <filip.grajek@gmail.com>
2021-12-04 01:23:00 +0100

    Solution for 03b

--- advent03/Program.cs
+++ advent03/Program.cs
@@ -1,21 +1,13 @@
 ﻿var binaries = File.ReadAllLines("inputa.txt");
-
+const int ZeroAsciiCode = 48;
+//a
 var length = binaries.First().Length;
 int[][] counts = new int[length][];
-for(int i = 0; i < length; i++)
+for (int i = 0; i < length; i++)
 {
-    counts[i] = new int[2];
+    counts[i] = GetCountsForBit(binaries, i);
 }
 
-const int ZeroAsciiCode = 48;
-
-foreach (var binary in binaries)
-{
-    for(int i = 0; i < length; i++)
-    {
-        counts[i][binary[i] - ZeroAsciiCode]++;
-    }
-}
 
 var gamma = 0L;
 var epsilon = 0L;
@@ -37,4 +29,66 @@ Console.WriteLine(Convert.ToString(gamma, 2).PadLeft(length));
 Console.WriteLine(Convert.ToString(epsilon, 2).PadLeft(length));
 Console.WriteLine(gamma*epsilon);
 
+
+//b
+
+var oxygenCandidates = binaries.ToList();
+var oxygenString = GetRating(oxygenCandidates, OxygenCriterium);
+var oxygenRating = Convert.ToInt64(oxygenString, 2);
+
+var co2Candidates = binaries.ToList();
+var co2String = GetRating(co2Candidates, CO2Criterium);
+var co2Rating = Convert.ToInt64(co2String, 2);
+
+Console.WriteLine(oxygenString);
+Console.WriteLine(co2String);
+Console.WriteLine(oxygenRating * co2Rating);
+
+bool OxygenCriterium(char bit, int[] curCounts)
+{
+    var maxIndex = 1;
+    if(curCounts[0] > curCounts[maxIndex])
+    {
+        maxIndex = 0;
+    }
+    return (bit - ZeroAsciiCode) == maxIndex;
+}
+
+bool CO2Criterium(char bit, int[] curCounts)
+{
+    var minIndex = 0;
+    if (curCounts[1] < curCounts[minIndex])
+    {
+        minIndex = 1;
+    }
+    return (bit - ZeroAsciiCode) == minIndex;
+}
+
+string GetRating(IEnumerable<string> candidates, Func<char, int[], bool> bitCriterium)
+{
+    for(int i = 0; i < counts.Length; i++)
+    {
+        if(candidates.Count() == 1)
+        {
+            return candidates.Single();
+        }
+
+        candidates = candidates.Where(c => bitCriterium(c[i], GetCountsForBit(candidates, i))).ToList();
+
+        //Console.WriteLine(string.Join(", ", candidates));
+    }
+
+    return candidates.Single();
+}
+
+int[] GetCountsForBit(IEnumerable<string> candidates, int i)
+{
+    var result = new int[2];
+    foreach (var candidate in candidates)
+    {
+        result[candidate[i] - ZeroAsciiCode]++;
+    }
+
+    return result;
+}
 
